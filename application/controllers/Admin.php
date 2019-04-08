7bc3ff8307a3a290a63bd14b65a61cdb0194e402
luisxxor <luisxxor@gmail.com>
2019-04-08 13:12:32 -0400

    Added captcha to login and a wallpaper as background

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -9,8 +9,7 @@ class Admin extends CI_Controller {
     $this->load->model('User_model', 'user');
   }
 
-  public function index()
-  {
+  public function index() {
     if (!$this->session->has_userdata('is_authenticated')) {
     	$this->load->view('admin/login');
     }
@@ -21,32 +20,44 @@ class Admin extends CI_Controller {
     }
   }
 
-  public function process_login()
-  {
-    $sessArray = array();
+  public function process_login() {
 
-    $username = $this->input->post('username');
-    $password = $this->input->post('password');
+    $recaptcha = $this->checkRecaptcha($this->input->post('g-recaptcha-response'));
 
-    $this->user->setUsername($username);
-    $this->user->setPassword($password);
+    $recaptcha = json_decode($recaptcha,true);
 
-    $result = $this->user->login();
+    if($recaptcha['success'])
+    {
+      $sessArray = array();
 
-    if($result) {
-      foreach($result as $row) {
-        $sessArray = array(
-          'id' => $row->id,
-          'username' => $row->username,
-          'is_authenticated' => TRUE,
-        );
-        $this->session->set_userdata($sessArray);
-        var_dump($this->session->userdata());
+      $username = $this->input->post('username');
+      $password = $this->input->post('password');
+
+      $this->user->setUsername($username);
+      $this->user->setPassword($password);
+
+      $result = $this->user->login();
+
+      if($result) {
+        foreach($result as $row) {
+          $sessArray = array(
+            'id' => $row->id,
+            'username' => $row->username,
+            'is_authenticated' => TRUE,
+          );
+          $this->session->set_userdata($sessArray);
+          var_dump($this->session->userdata());
+        }
+        redirect('admin');
+      } else {
+        redirect('admin/?msg=1');
       }
-      redirect('admin');
-    } else {
-      redirect('admin/?msg=1');
     }
+    else
+    {
+      redirect('admin/?msg=2');
+    }
+
   }
 
 	public function logout() {
@@ -58,4 +69,38 @@ class Admin extends CI_Controller {
 		$this->output->set_header("Pragma: no-cache");
 		redirect('admin');
 	}
+
+  private function getRealIpAddr() {
+    if (!empty($_SERVER['HTTP_CLIENT_IP']))
+    {
+      $ip=$_SERVER['HTTP_CLIENT_IP'];
+    }
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+    {
+      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    else
+    {
+      $ip=$_SERVER['REMOTE_ADDR'];
+    }
+
+    return $ip;
+  }
+
+  private function checkRecaptcha($response) {
+    $curl = curl_init();
+
+    $data = array('secret' => '6LdXvJsUAAAAAKEIQrjmJbIuQVz8JO2ZVAcmEDL6', 'response' => $response, 'remoteip' => $this->getRealIpAddr());
+
+    curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+    curl_setopt($curl, CURLOPT_URL, "https://www.google.com/recaptcha/api/siteverify");
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $result = curl_exec($curl);
+
+
+    curl_close($curl);
+
+    return $result;
+  }
+
 }
--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -10,20 +10,90 @@
     <title>Login</title>
     <style>
       body {
-        background: #83a4d4;
-        background: -webkit-linear-gradient(to left, #b6fbff, #83a4d4);
-        background: linear-gradient(to left, #b6fbff, #83a4d4);
+        background-image: url("<?PHP echo base_url('assets/images/loginwallpaper.jpg'); ?>");
         min-height: 100vh;
         max-width: 100vw;
         display: flex;
         flex-wrap: wrap;
         align-items: center;
+        background-repeat: no-repeat;
+        background-position: right bottom;
+        background-color: #FFFFFF;
+        background-size: contain;
       }
 
       .section {
         flex: 1 1 100%;
       }
+
+      .only-danger {
+        display: none;
+      }
+
+      .is-danger ~ .only-danger {
+        display: block;
+      }
     </style>
+   
+    <script>
+      
+      function onSubmit(event) {
+        const $form = document.getElementById("login_form");
+        let user = checkUser();
+        let pass = checkPass();
+        if(user && pass)
+        {
+          event.preventDefault();
+          grecaptcha.execute();
+        }
+      }
+
+      function sendForm(token) {
+        const $form = document.getElementById("login_form");
+        form.submit();
+      }
+
+      function checkUser() {
+        hideMessage();
+        const $user = document.getElementById("username");
+        let user_is_valid = $user.checkValidity();
+
+        if(user_is_valid) {
+          $user.classList.remove('is-danger');
+        }
+        else {
+          $user.classList.add('is-danger');
+        }
+
+        return user_is_valid;
+      }
+
+      function checkPass() {
+        hideMessage();
+        const $pass = document.getElementById("password");
+        let pass_is_valid = $pass.checkValidity()
+
+        if(pass_is_valid) {
+          $pass.classList.remove('is-danger');
+        }
+        else {
+          $pass.classList.add('is-danger');
+        }
+
+        return pass_is_valid;
+      }
+
+      function hideMessage() {
+        document.querySelector('.message').style.display = "none";
+      }
+
+      window.addEventListener('load',function() {
+        document.getElementById('username').addEventListener('input',checkUser);
+        document.getElementById('password').addEventListener('input',checkPass);
+      });
+    </script>
+
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
 <body>
   <section class="section">
@@ -33,24 +103,47 @@
           <div class="card">
             <div class="card-content">
               <div class="content">
-                <form action="<?PHP echo base_url('admin/process_login') ?>" method="POST">
+                <form id="login_form" action="<?PHP echo base_url('admin/process_login') ?>" method="POST">
                   <div class="field">
-                    <p class="control has-icons-left">
-                      <input class="input" id="username" type="text" name="username" placeholder="Nombre de usuario"/>
+                    <div class="control has-icons-left">
+                      <input class="input" id="username" type="text" name="username" required placeholder="Nombre de usuario"/>
                         <span class="icon is-small is-left">
                           <i class="fas fa-user"></i>
                         </span>
-                    </p>
+                      <p class="help is-danger only-danger">El nombre de usuario no puede estar vacío.</p>
+                    </div>
                   </div>
                   <div class="field">
-                    <p class="control has-icons-left">
-                      <input class="input" type="password" id="password" name="password" placeholder="********"/>
+                    <div class="control has-icons-left">
+                      <input class="input" type="password" id="password" name="password" required placeholder="********"/>
                       <span class="icon is-small is-left">
                         <i class="fas fa-lock"></i>
                       </span>
-                    </p>
+                      <p class="help is-danger only-danger">La contraseña no puede estar vacía.</p>
+                    </div>
                   </div>
+                  <?PHP if(isset($_GET['msg'])): ?>
+                    <?PHP if($_GET['msg'] == 1): ?>
+                      <article class="message is-danger">
+                        <div class="message-body">
+                          Usuario y/o contraseña incorrectos.
+                        </div>
+                      </article>
+                    <?PHP elseif($_GET['msg'] == 2): ?>
+                      <article class="message is-danger">
+                        <div class="message-body">
+                          Error con el Re-captcha.
+                        </div>
+                      </article>
+                    <?PHP endif; ?>
+                  <?PHP endif; ?>
 
+                  <div class="g-recaptcha"
+                    data-sitekey="6LdXvJsUAAAAAAX91zwKh0J7QEaKbYF76eME_uto"
+                    data-callback="onSubmit"
+                    data-size="invisible">
+                  </div>
+                  <input type="hidden" id="captcha" name="captcha" value="">
                   <div class="field">
                     <div class="control">
                       <button class="button is-link">Iniciar sesión</button>
